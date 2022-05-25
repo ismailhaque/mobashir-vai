9eca2c88e8a2f2ba5dfe2f0a789136e4adad740e
ismailhaque <ismailhaque2956@gmail.com>
2022-05-25 18:28:54 +0600

    coures page done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // get element
+// Accordion js
 const header = document.querySelectorAll(`.faq_header`);
 
 header.forEach(item => {
@@ -14,4 +15,25 @@ header.forEach(item => {
         item.nextElementSibling.classList.toggle(`active`);
         item.lastElementChild.innerHTML=`-`;
     })
+});
+// Accordion js end
+// search_option js
+const search_option_show = document.querySelector(`#search_option_show`);
+const search_option_close = document.querySelector(`#search_option_close`);
+const category_search_option = document.querySelector(`#category_search_option`);
+const shop_all_coures = document.querySelector(`#shop_all_coures`);
+
+search_option_show.addEventListener(`click`, function(){
+    category_search_option.classList.add(`active`);
+    shop_all_coures.classList.add(`active`);
+    search_option_show.classList.add(`active`);
+    search_option_close.classList.remove(`active`);
+});
+
+search_option_close.addEventListener(`click`, function(){
+
+    search_option_show.classList.remove(`active`);
+    category_search_option.classList.remove(`active`);
+    shop_all_coures.classList.remove(`active`);
+    search_option_close.classList.add(`active`);
 });

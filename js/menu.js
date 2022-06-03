3845541a3d7db3aa5e7e6963bdf0158a99446fd4
ismailhaque <ismailhaque2956@gmail.com>
2022-06-03 17:05:06 +0600

    login page complated

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,9 @@
 const responsive_menu_show = document.querySelector(`.responsive_menu_show`);
 const responsive_menu_close = document.querySelector(`.menu-close`);
 const responsive_header = document.querySelector(`.responsive_header`);
+const menu_cart = document.querySelector(`.menu_cart`);
+const shopping_cart = document.querySelector(`.shopping-cart`);
+const responsive_shopping_cart = document.querySelector(`.responsive_shopping_cart`);
 
 responsive_menu_show.addEventListener(`click`, function () {
     responsive_menu_show.classList.add(`active`);
@@ -12,5 +15,13 @@ responsive_menu_close.addEventListener(`click`, function () {
     responsive_header.classList.remove(`active`);
     responsive_menu_show.classList.remove(`active`);
     responsive_menu_close.classList.remove(`active`);
+    menu_cart.classList.remove(`active`);
 
 });
+
+shopping_cart.addEventListener(`click`, function () {
+    menu_cart.classList.toggle(`active`);
+})
+responsive_shopping_cart.addEventListener(`click`, function () {
+    menu_cart.classList.toggle(`active`);
+})
